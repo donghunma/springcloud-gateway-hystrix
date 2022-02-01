24acbcc54b4fa879594c043875cb182d45aff73d
마동훈 <rewritable.mind@gmail.com>
2022-02-01 12:01:08 +0900

    daily commit

--- cloud-config-server/src/main/java/com/rapidstar/cloud/config/CloudConfigServerApplication.java
+++ cloud-config-server/src/main/java/com/rapidstar/cloud/config/CloudConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.javatechie.cloud.config;
+package com.rapidstar.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- cloud-config-server/src/test/java/com/rapidstar/cloud/config/CloudConfigServerApplicationTests.java
+++ cloud-config-server/src/test/java/com/rapidstar/cloud/config/CloudConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.javatechie.cloud.config;
+package com.rapidstar.cloud.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
